9b04f26f1d0058ee07f9c6cac34a4f10bf5cd686
Jeeva <selvijeeva.a@gmail.com>
2023-10-17 14:49:47 +1100

    First Commit

---
+++ Features/Feature1.feature
@@ -0,0 +1,15 @@
+﻿Feature: Youtube search feature
+
+search for a video with keyword
+
+@TestersTalk
+Scenario: Search for TestersTalk
+	Given Open the browser
+	When Enter the URL
+	Then Search for TestersTalk
+
+	@TestersTalk
+Scenario: Search for TestersTalk2
+	Given Open the browser
+	When Enter the URL
+	Then Search for TestersTalk
---
+++ StepDefinitions/FeatureStepDefinitions.cs
@@ -0,0 +1,36 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SpecFlowProject1.StepDefinitions
+{
+    [Binding]
+    public sealed class FeatureStepDefinitions
+    {
+        private IWebDriver driver;
+
+        [Given(@"Open the browser")]
+        public void GivenOpenTheBrowser()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [When(@"Enter the URL")]
+        public void WhenEnterTheURL()
+        {
+            driver.Url = "https://www.youtube.com/";
+        }
+
+        [Then(@"Search for TestersTalk")]
+        public void ThenSearchForTestersTalk()
+        {
+            driver.FindElement(By.XPath("//input[@id='search']")).SendKeys("Testers Talk");
+            driver.FindElement(By.XPath("//input[@id='search']")).SendKeys(Keys.Enter);
+
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+
+
+    }
+}
